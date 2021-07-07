57a573d98e4da1939103a3269dc2d0cf5ddc0a74
agrinJPG <420757797@qq.com>
2021-07-07 15:44:23 +0800

    update style of course

--- vue-admin-template/src/views/edu/subject/list.vue
+++ vue-admin-template/src/views/edu/subject/list.vue
@@ -8,18 +8,21 @@
       :props="defaultProps"
       :filter-node-method="filterNode"
       class="filter-tree"
+      :render-content="renderContent"
       default-expand-all
-    >
-    </el-tree>
+    />
 
   </div>
 </template>
 <style>
-.el-tree{
-  color: rgb(5, 5, 5);
-  font-weight: bolder;
-}
-
+.custom-tree-node {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding-right: 8px;
+  }
 </style>
 
 <script>
@@ -57,8 +60,18 @@ export default {
         .then(response=>{
             this.data2=response.data.subjectList
         })
-    }
+    },
 
+    renderContent(h, { node, data, store }) {
+        return (
+          <span class="custom-tree-node">
+            <span>{node.label}</span>
+            <span>
+              <el-button size="mini" type="text" on-click={ () => this.append(data) }>Append</el-button>
+              <el-button size="mini" type="text" on-click={ () => this.remove(node, data) }>Delete</el-button>
+            </span>
+          </span>);
+    }
 
 
 
