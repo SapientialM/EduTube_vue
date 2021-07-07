b6e685feb060b5900c30dad93a66881e2299277c
agrinJPG <420757797@qq.com>
2021-07-07 11:35:27 +0800

    查询最新操作记录

--- vue-admin-template/src/views/dashboard/index.vue
+++ vue-admin-template/src/views/dashboard/index.vue
@@ -8,14 +8,10 @@
     <div class="body">
       <div class="info">
         <div class="pic">
-          <img
-            :src="avatar + '?imageView2/1/w/80/h/80'"
-            class="user-avatar"
-            style="border-radius: 50%"
-          />
+          <img :src="avatar + '?imageView2/1/w/80/h/80'" class="user-avatar" />
         </div>
         <div class="intro">
-          <div class="dashboard-text">{{ name }}</div>
+          <div class="dashboard-text">name:{{ name }}</div>
           <div class="dashboard-text roles">
             roles:<span v-for="role in roles" :key="role">{{ role }}</span>
           </div>
@@ -48,16 +44,12 @@
         <h3>消息通知</h3>
         <el-collapse v-model="activeName" accordion>
           <!-- 读数据失败 -->
-          <el-collapse-item
-            v-for="item in messageData"
-            :key="item.id"
-            :title="item.title + ' ' + item.time"
-            :name="item.id"
-          >
+          <el-collapse-item v-for="item in messageData" :key="item.id" :title="item.title+' '+item.time" :name="item.id" >
             <div>
               {{ item.content }}
             </div>
           </el-collapse-item>
+          
         </el-collapse>
       </div>
     </div>
@@ -72,14 +64,14 @@
           <dd><a href="#">帮助</a></dd>
         </dl>
         <dl>
-          <dt>关于 EduTube</dt>
+          <dt>关于学成网</dt>
           <dd><a href="#">管理团队</a></dd>
           <dd><a href="#">工作机会</a></dd>
           <dd><a href="#">客户服务</a></dd>
           <dd><a href="#">帮助</a></dd>
         </dl>
         <dl>
-          <dt>关于 EduTube</dt>
+          <dt>关于学成网</dt>
           <dd><a href="#">管理团队</a></dd>
           <dd><a href="#">工作机会</a></dd>
           <dd><a href="#">客户服务</a></dd>
@@ -115,7 +107,7 @@ export default {
           sender: "others",
           accepted: "admin",
           content:
-            "与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。",
+          "与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。",
         },
         {
           id: "2",
@@ -125,7 +117,7 @@ export default {
           sender: "others",
           accepted: "admin",
           content:
-            "与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。",
+          "与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。",
         },
       ],
     };
@@ -144,12 +136,12 @@ export default {
   },
   methods:{
     getHistory(){
-      history.getHistory().then((res) => {
+      history.getNewHistory().then((res) => {
         // console.log(res)
         // this.operationData = res.data;
-        this.operationList =  res.data.history;
+        this.operationList =  res.data.newhistory;
         console.log(this.operationList.length);
-        for(var i=0;i<10;i++){
+        for(var i=0;i<this.operationList.length;i++){
           this.operationData.push(this.operationList[i]);
         }
       })
@@ -261,6 +253,6 @@ export default {
   order: -1;
   padding: 0 20px;
   font-weight: 900;
-  color: rgb(104, 187, 243);
+  color: #666;
 }
 </style>
