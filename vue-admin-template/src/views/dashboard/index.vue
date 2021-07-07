47f368c5a478e6e40c0b86e63b00a839fd83c61c
agrinJPG <420757797@qq.com>
2021-07-07 09:40:08 +0800

    debug

--- vue-admin-template/src/views/dashboard/index.vue
+++ vue-admin-template/src/views/dashboard/index.vue
@@ -54,34 +54,12 @@
         <h3>消息通知</h3>
         <el-collapse v-model="activeName" accordion>
           <!-- 读数据失败 -->
-          <el-collapse-item title="一致化" name="1">
+          <el-collapse-item v-for="item in messageData" :key="item.id" :title="item.title+' '+item.time" :name="item.id" >
             <div>
-              控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；
+              {{ item.content }}
             </div>
           </el-collapse-item>
-          <el-collapse-item title="反馈" name="2">
-            <div>
-              控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；
-            </div>
-            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-          </el-collapse-item>
-          <el-collapse-item title="效率" name="3">
-            <div>简化流程：设计简洁直观的操作流程；</div>
-            <div>
-              清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；
-            </div>
-            <div>
-              帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。
-            </div>
-          </el-collapse-item>
-          <el-collapse-item title="可控" name="4">
-            <div>
-              用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；
-            </div>
-            <div>
-              结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。
-            </div>
-          </el-collapse-item>
+          
         </el-collapse>
       </div>
     </div>
@@ -113,11 +91,24 @@ export default {
       ],
       messageData: [
         {
+          id: "1",
           title: "通知",
+          time: "2021/07/07 08:37:01",
+          isread: false,
           sender: "others",
           accepted: "admin",
           content:
-            "与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。",
+          "与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。",
+        },
+        {
+          id: "2",
+          title: "通知",
+          time: "2021/07/07 08:40:02",
+          isread: false,
+          sender: "others",
+          accepted: "admin",
+          content:
+          "与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。",
         },
       ],
     };
